20c8330c2792c28886a8ed59cc4be9270a654519
Zartab Nakhwa <zartab@codewithz.com>
2024-07-16 12:15:57 +0530

    @JSonIgnore- Final

---
+++ spring-boot-playground/spring-boot-playground/src/main/java/com/inorg/model/Person.java
@@ -0,0 +1,90 @@
+package com.inorg.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(value = {"password"},allowSetters = true,allowGetters = false)
+public class Person {
+
+    @JsonProperty("aadhar_number")
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String email;
+    private String password;
+
+    public Person() {
+    }
+
+    public Person(int id, String firstName,String lastName ,int age, String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.age = age;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", password='" + getPassword() + '\'' +
+                '}';
+    }
+}
