4cfad203d4ce5ed8ef10822b84e714ecb51c266b
Zartab Nakhwa <zartab@codewithz.com>
2024-07-18 10:09:27 +0530

    JPA:Implemented Faker and generated random students

--- spring-boot-playground/spring-boot-playground/src/main/java/com/inorg/SpringBootPlaygroundApplication.java
+++ spring-boot-playground/spring-boot-playground/src/main/java/com/inorg/SpringBootPlaygroundApplication.java
@@ -1,7 +1,10 @@
 package com.inorg;
 
+import com.github.javafaker.Faker;
 import com.inorg.model.Customer;
+import com.inorg.model.Student;
 import com.inorg.repository.CustomerRepository;
+import com.inorg.repository.StudentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,10 +26,26 @@ public class SpringBootPlaygroundApplication {
 	}
 
 	@Bean
-	CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
+	CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
 		return args -> {
-
+				generateRandomStudents(studentRepository);
 		};
+	}
+
+	private void generateRandomStudents(StudentRepository studentRepository) {
+
+		Faker faker = new Faker();
+
+		for(int index=1;index<=50;index++){
+
+			String firstName = faker.name().firstName();
+			String lastName = faker.name().lastName();
+			String email=String.format("%s@gmail.com",firstName+"."+lastName);
+			int age=faker.number().numberBetween(18,25);
+			Student student=new Student(firstName,lastName,email,age);
+			studentRepository.save(student);
+		}
+
 	}
 
 }
--- spring-boot-playground/spring-boot-playground/src/main/java/com/inorg/model/Student.java
+++ spring-boot-playground/spring-boot-playground/src/main/java/com/inorg/model/Student.java
@@ -57,8 +57,8 @@ public class Student {
     public Student() {
     }
 
-    public Student(Long id, String firstName, String lastName, String email, Integer age) {
-        this.id = id;
+    public Student(String firstName, String lastName, String email, Integer age) {
+
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
