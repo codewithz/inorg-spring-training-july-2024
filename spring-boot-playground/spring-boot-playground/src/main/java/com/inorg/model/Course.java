8ee7d739466c7501d645dc7caa1eb01a33d09ddd
Zartab Nakhwa <zartab@codewithz.com>
2024-07-18 13:04:03 +0530

    JPA:ManyToMany Added Course Entity

---
+++ spring-boot-playground/spring-boot-playground/src/main/java/com/inorg/model/Course.java
@@ -0,0 +1,78 @@
+package com.inorg.model;
+
+import jakarta.persistence.*;
+
+@Entity(name="Course")
+@Table(name="course")
+public class Course {
+
+    @Id
+    @SequenceGenerator(
+            name = "course_sequence",
+            sequenceName = "course_sequence",
+            allocationSize = 1,
+            initialValue = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "course_sequence"
+    )
+    @Column(
+            name="id",
+            updatable = false
+    )
+    Long id;
+    @Column(
+            name = "name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    String name;
+    @Column(
+            name = "department",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    String department;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Course() {
+    }
+
+    public Course(String name, String department) {
+        this.name = name;
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                '}';
+    }
+}
